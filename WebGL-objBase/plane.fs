4ec9b316288d14be6c5c5c0cad470d789e1e44ce
Maxo988 <maelzamitb@gmail.com>
2026-09-01 12:06:22 +0200

    Load texture

---
+++ WebGL-objBase/plane.fs
@@ -0,0 +1,13 @@
+
+precision mediump float;
+
+varying vec2 texCoords;
+uniform sampler2D uSampler;
+
+void main(void)
+{
+  gl_FragColor = texture2D(uSampler, texCoords);
+}
+
+
+
---
+++ gui.js
@@ -0,0 +1,164 @@
+function rotate() {}
+
+var gui = {
+  vitesse: { value: 1.0, min: 1.0, max: 10.0, step: 0.2, text: "Vitesse" },
+};
+
+var lspc = "\xa0".repeat(10);
+var llspc = "\xa0".repeat(40);
+var rotX = 0;
+var rotY = 0;
+var trX = 0;
+var trY = 0;
+
+//=============================================================
+function initGui() {
+  var body = document.getElementsByClassName("gui")[0];
+
+  var table = document.createElement("table");
+  body.appendChild(table);
+
+  gui.guiBody = document.createElement("tbody");
+  table.appendChild(gui.guiBody);
+
+  sec = gui_section("Scene Options");
+  gui_slider(sec, gui.spacing);
+  gui_vspace(sec);
+  gui_slider(sec, gui.transp);
+  gui_slider(sec, gui.hotpoint);
+  gui_vspace(sec);
+  gui_text(sec, "Choix des couleurs du Hot Point");
+  gui_text(sec, "N'EN SELECTIONNER QU'UNE SEULE !");
+  gui_vspace(sec);
+  gui_checkbox(sec, gui.hotpoint_magenta);
+  gui_checkbox(sec, gui.hotpoint_green);
+  gui_checkbox(sec, gui.hotpoint_yellow);
+  gui_vspace(sec);
+  gui_slider(sec, gui.thresL);
+  gui_slider(sec, gui.thresH);
+  gui_vspace(sec);
+  gui_slider(sec, gui.depth);
+  gui_slider(sec, gui.plane);
+  gui_vspace(sec);
+  gui_checkbox(sec, gui.checkbox);
+  gui_vspace(sec);
+  gui_slider(sec, gui.vitesse);
+
+  sec = gui_section("Objects");
+  insert(sec, "serie01/view.png", 1);
+  insert(sec, "serie02/view.png", 2);
+  insert(sec, "serie03/view.png", 3);
+  insert(sec, "serie04/view.png", 4);
+  insert(sec, "serie05/view.png", 5);
+  insert(sec, "serie06/view.png", 6);
+}
+
+//=============================================================
+function insert(sec, fimg, val) {
+  var img = document.createElement("img");
+  img.src = fimg;
+  img.setAttribute("width", "122");
+  img.setAttribute("height", "122");
+  img.addEventListener("click", (e) => {
+    gui.serie.value = val;
+  });
+  sec.appendChild(img);
+  sec.appendChild(document.createTextNode(" "));
+}
+
+//=============================================================
+function gui_section(title) {
+  var tr = document.createElement("tr");
+  gui.guiBody.appendChild(tr);
+
+  var td = document.createElement("td");
+  td.appendChild(document.createTextNode(title));
+  tr.appendChild(td);
+
+  var tr = document.createElement("tr");
+  gui.guiBody.appendChild(tr);
+  td = document.createElement("td");
+  tr.appendChild(td);
+
+  return td;
+}
+
+//=============================================================
+function gui_vspace(sec) {
+  var par = document.createElement("p");
+  par.className = "p";
+  sec.appendChild(par);
+}
+
+//=============================================================
+function gui_text(sec, text) {
+  sec.appendChild(document.createTextNode(text));
+  sec.appendChild(document.createElement("br"));
+}
+
+//=============================================================
+function gui_info(sec, obj) {
+  obj.id = document.createTextNode("");
+  gui_info_update(obj);
+  sec.appendChild(obj.id);
+  sec.appendChild(document.createElement("br"));
+}
+
+//=============================================================
+function gui_info_update(obj) {
+  obj.id.nodeValue = obj.text + ": " + obj.value + " " + obj.unit;
+}
+
+//=============================================================
+function gui_checkbox(sec, obj) {
+  var box = document.createElement("input");
+  box.type = "checkbox";
+  box.className = "checkbox";
+  box.checked = obj.value;
+  box.addEventListener("change", function () {
+    obj.value = box.checked;
+    if (obj.callback) obj.callback(box.checked);
+  });
+
+  var label = document.createElement("label");
+  label.innerHTML = obj.text;
+  label.className = "label";
+
+  sec.appendChild(box);
+  sec.appendChild(label);
+  sec.appendChild(document.createElement("br"));
+}
+
+//=============================================================
+function gui_slider(sec, obj) {
+  var slider = document.createElement("input");
+  slider.type = "range";
+  slider.className = "slider";
+  slider.min = obj.min;
+  slider.max = obj.max;
+  slider.value = obj.value;
+  slider.step = obj.step;
+  slider.id = obj.text;
+  obj.slider = slider;
+
+  txt = document.createTextNode(obj.text + " : " + slider.value);
+  obj.slider.txt = txt;
+
+  obj.slider.addEventListener("input", function () {
+    obj.value = obj.slider.value;
+    obj.slider.txt.nodeValue = slider.id + ": " + slider.value;
+  });
+
+  sec.appendChild(txt);
+  sec.appendChild(slider);
+  sec.appendChild(document.createElement("br"));
+}
+
+//=============================================================
+function gui_slider_update(obj, val) {
+  obj.value = val;
+  obj.slider.txt.nodeValue = obj.text + " : " + val;
+  obj.slider.value = val;
+}
+
+//
